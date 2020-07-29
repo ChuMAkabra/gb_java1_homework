2859346b660e39c6c772fbe53aefd98379bb2587
Dmitry Chumanov <dmitry.chumanov@gmail.com>
2020-07-29 22:25:28 +0300

    Lesson 3 homework skeleton

---
+++ src/main/java/Assignment_03/IrregularVerbs.java
@@ -0,0 +1,8 @@
+package Assignment_03;
+
+public class IrregularVerbs {
+    private final String[] words = {"be","was","been","bear","bore","born","beat","beat","beaten",
+            "become","became","become","begin","began","begun","bind","bound","bound"};
+
+
+}
