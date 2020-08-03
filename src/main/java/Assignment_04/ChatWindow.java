a2b7d3c98e206d9f86a7fc5b1c1cc22272366988
Dmitry Chumanov <dmitry.chumanov@gmail.com>
2020-08-03 16:21:09 +0300

    Added comments. Removed one line for insets. Renamed button object. Now empty message cannot be sent

--- src/main/java/Assignment_04/ChatWindow.java
+++ src/main/java/Assignment_04/ChatWindow.java
@@ -22,17 +22,17 @@ public class ChatWindow extends JFrame {
         // создание текстового поля (и скролл-баров)
         chatText = new JTextArea();
         chatText.setBackground(new Color(230,230,230));
-        chatText.setEditable(false);
+        chatText.setEditable(false); // теперь текст нельзя редактировать вручную
         JScrollPane chatScroll = new JScrollPane(chatText);
 
         // установка ограничений текстового поля чата
         GridBagConstraints constraints = new GridBagConstraints();
         constraints.fill = GridBagConstraints.BOTH;
-        constraints.weightx = 1;
-        constraints.weighty = 0.98;
-        constraints.gridx = 0;
-        constraints.gridy = 0;
-        constraints.gridwidth = 2;
+        constraints.weightx = 1;    // занимает 100% по горизонтали
+        constraints.weighty = 0.98; // занимает  98% по вертикали
+        constraints.gridx = 0;      // начинается в  первой колонке
+        constraints.gridy = 0;      // начинается на первой строке
+        constraints.gridwidth = 2;  // занимает обе колонки первой строки
         add(chatScroll, constraints);
 
         // создание однострочного поля ввода (и скролл-баров)
@@ -41,37 +41,40 @@ public class ChatWindow extends JFrame {
 
         // установка ограничений однострочного поля ввода
         JScrollPane editScroll = new JScrollPane(editText);
-        constraints.weightx = 0.99;
-        constraints.weighty = 0.02;
-        constraints.gridwidth = 1;
-        constraints.gridx = 0;
-        constraints.gridy = 1;
-        constraints.insets = new Insets(5,0,5,0);
+        constraints.weightx = 1;    // занимает  100% по горизонтали
+        constraints.weighty = 0.02; // занимает   2% по вертикали
+        constraints.gridx = 0;      // начинается в  первой колонке
+        constraints.gridy = 1;      // начинается на второй строке
+        constraints.gridwidth = 1;  // занимает одну колонку второй строки
+        constraints.insets = new Insets(5,0,5,0); // отступы от краев области
         add(editScroll, constraints);
 
         // создание кнопки
-        JButton enterButton = new JButton("Send");
-        enterButton.addActionListener(new sendTextActionListener());
+        JButton sendButton = new JButton("Send");
+        sendButton.addActionListener(new sendTextActionListener());
 
         // установка ограничений кнопки
-        constraints.gridx = 1;
-        constraints.weightx = 0.01;
-        constraints.weighty = 0;
-        constraints.fill = GridBagConstraints.NONE;
-        constraints.anchor = GridBagConstraints.CENTER;
-        constraints.insets = null;
-        constraints.insets = new Insets(0,0,0,0);
-        add(enterButton, constraints);
+        constraints.weightx = 0;    // занимает 0% по горизонтали (не меняет размер при расширении)
+        constraints.weighty = 0;    // занимает 0% по вертикали (не меняет размер при расширении)
+        constraints.gridx = 1;      // начинается во второй колонке
+        constraints.fill = GridBagConstraints.NONE;     // не меняет размер при расширении
+        constraints.anchor = GridBagConstraints.CENTER; // располагается по центру своей области
+        constraints.insets = new Insets(0,0,0,0); // не имеет отступов
+        add(sendButton, constraints);
 
         setVisible(true);
     }
 
+    // обработчик события отправки сообщений (по нажатии Enter или кнопки Send)
     public class sendTextActionListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
-            String chat = chatText.getText();
-            chatText.setText(chat + (!chat.equals("") ? "\n" : "") + "Me:\n" + editText.getText());
-            editText.setText("");
+            String edit = editText.getText();
+            if (!edit.equals("")) {
+                String chat = chatText.getText();
+                chatText.setText(chat + (!chat.equals("") ? "\n" : "") + "Me:\n" + editText.getText());
+                editText.setText("");
+            }
         }
     }
 }
